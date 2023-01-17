e05904753c0bfd97e3d7cae8a0268e2ae56a9f4e
James-Kelley-Galvanize <james.kelley@galvanize.com>
2023-01-17 08:47:03 -0700

    adjust boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+const url = process.env.REACT_APP_API_URL || `localhost:8080/`;
 
 function App() {
+  const [apiDependantState, setApiDependantState] = useState({
+    message: `NO API CONNECTION LOADED YET`,
+  });
+  const [dbDependantState, setDbDependantState] = useState({
+    message: `NO DB DATA RETRIEVED FROM API YET`,
+  });
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setApiDependantState(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+  useEffect(() => {
+    fetch(`${url}cats`)
+      .then((res) => res.json())
+      .then((data) => {
+        setDbDependantState(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [apiDependantState]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Experimental Heroku Client</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1> API CONNECTION INFO </h1>
+          {JSON.stringify(apiDependantState)}
+          <h1> DB FUNCTION INFO </h1>
+          {JSON.stringify(dbDependantState)}
         </a>
       </header>
     </div>
