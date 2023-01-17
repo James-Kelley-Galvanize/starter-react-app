22c76c99e2802fc7a1871feb33c0b1bb86fe772e
James-Kelley-Galvanize <james.kelley@galvanize.com>
2023-01-17 10:47:32 -0700

    change Heroku to Cyclic in app.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Experimental Heroku Client</p>
+        <p>Experimental Cyclic Client</p>
         <a
           className="App-link"
           href="https://reactjs.org"
